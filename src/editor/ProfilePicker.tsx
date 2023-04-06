243314d3a0fd6287ec9acb8b9dfc511ecd39811a
shaoster <shaoster@gmail.com>
2023-04-06 14:24:34 -0400

    Make the profile picker a bit clearer and uglier.

--- src/editor/ProfilePicker.tsx
+++ src/editor/ProfilePicker.tsx
@@ -1,10 +1,12 @@
 // https://github.com/shaoster/space-station/new/main/game-configurations
 
-import { Box, Grid, Typography } from "@mui/material";
+import DeleteIcon from '@mui/icons-material/Delete';
+import { Box, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Grid } from "@mui/material";
 import { ReactNode } from "react";
 import { HashRouter, Link, Route, Routes, useParams } from "react-router-dom";
-import { GameConfigurationProvider } from "./Util";
 import { GameConfiguration } from "../glossary/Compendium";
+import { GameConfigurationProvider } from "./Util";
+import React from 'react';
 
 const context = require.context("../configurations", true, /.json$/);
 export const STATIC_PROFILES: {[key: string]: GameConfiguration} = {};
@@ -26,37 +28,86 @@ export const ProfileNameProvider = ({children} : {children: ReactNode}) => {
   </GameConfigurationProvider>;
 };
 
+const EXPLANATION = 
+`The profiles listed here represent those statically tracked via source control.
+
+To create a new profile, click "Load to Edit" and then [POST PR TO GITHUB] in the summary panel.
+
+After the PR is merged and the new code deployed, a new profile should show up here.
+
+If you just want to edit stuff locally, just pick whichever profile you want and start editing.
+
+In that case, any changes are stored in your browser's local storage and will persist through page refreshes.
+
+However, if you want to share your game configuration with others, you should post a PR.
+`;
+
+const ProfileReverter = (
+  {profile} :
+  {profile: string}
+) => {
+  const [open, setOpen] = React.useState(false);
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleConfirm = () => {
+    localStorage.removeItem(profile);
+    handleClose();
+  }
+  return (<>
+    <Button onClick={()=>setOpen(true)} startIcon={<DeleteIcon />} variant="contained">
+      Revert Local Changes
+    </Button>
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+    >
+      <DialogTitle id="alert-dialog-title">
+        Are you really sure you want to revert?
+      </DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-description">
+          This removes any local edits you have made to this game configuration.
+          This deletion is permanent, and Phil cannot help you recover your data.
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose} variant="contained">Never Mind</Button>
+        <Button onClick={handleConfirm} variant="contained" autoFocus>
+          I'm sure
+        </Button>
+      </DialogActions>
+    </Dialog>
+  </>);
+}
+
 const ProfilePicker = () => {
   const profiles = Object.keys(STATIC_PROFILES);
   return <Box sx={{padding: 2}}>
-    <Typography>
-      The profiles listed here represent those statically tracked via source control.
-      <br/>
-      To create a new one, click "Load to Edit" and then [POST PR TO GITHUB] in the summary panel.
-      <br/> 
-      After the PR is merged and the new code deployed, a new profile should show up here.
-      <br/>
-      If you just want to edit stuff locally, just pick whichever profile you want and start editing.
-      <br/>
-      In that case, any changes are stored in your browser's local storage and will persist through page refreshes.
-      <br/>
-      However, if you want to share your game configuration with others, you should post a PR.
-    </Typography>
-    <Grid container sx={{padding: 2}}>
+    <pre>
+      {EXPLANATION}
+    </pre>
+    <Grid container sx={{padding: 2}} key={"profiles"}>
       {
-        profiles.map((p) => <>
-          <Grid item xs={1}>
-            {p}
+        profiles.map((p) => <Grid item key={p + "_row"} xs={12}>
+          <Grid container sx={{padding: 1, border: 1}}>
+            <Grid item xs={1} key={p + "_name"}>
+              {p}
+            </Grid>
+            <Grid item xs={1} key={p + "_play"}>
+              <Link to={`/${p}/play`}>Play</Link>
+            </Grid>
+            <Grid item xs={1} key={p + "_edit"}>
+              <Link to={`/${p}/edit`}>Load to Edit</Link>
+            </Grid>
+            <Grid item xs={2} key={p + "_revert"}>
+              <ProfileReverter profile={p}/>
+            </Grid>
+            <Grid item xs={7} key={p + "_gutter"}/>
           </Grid>
-          <Grid item xs={1}>
-            <Link to={`/${p}/play`}>Play</Link>
-          </Grid>
-          <Grid item xs={1}>
-            <Link to={`/${p}/edit`}>Load to Edit</Link>
-          </Grid>
-          <Grid item xs={9}>
-          </Grid>
-        </>)
+        </Grid>)
       }
     </Grid>
   </Box>;
